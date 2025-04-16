ad8746f996434fdf706e5ad97878c1b132000044
lpchi133 <lpchi21@clc.fitus.edu.vn>
2025-04-16 11:52:19 +0700

    refactor

--- lib/src/types/data.dart
+++ lib/src/types/data.dart
@@ -1,6 +1,4 @@
-import 'dart:ffi';
 import 'dart:typed_data';
-
 
 ///
 /// Class repesents one Data Row in a Data Field. MRTD additional data
@@ -25,29 +23,30 @@ class DataRow {
     this.tag = tag;
     this.value = value;
     // Calculate the length of `value` and convert it to a hexadecimal string.
-    this.length = value != null ? value.length : 0;
+    // ignore: unnecessary_null_comparison
+    length = value != null ? value.length : 0;
   }
 
   //convert all members to a hexadecimal Uint8List
   Uint8List toList() {
     // Create a Uint8List with a length of 4 + length of `value`.
+    // ignore: unnecessary_null_comparison
     final bytes = value != null ? Uint8List(2 + value.length) : Uint8List(2);
     bytes[0] = tag;
     bytes[1] = length;
     // Set the third and fourth bytes to `value`.
-    if (value != null)
-      bytes.setRange(2, 2 + value.length, value);
+    bytes.setRange(2, 2 + value.length, value);
     return bytes;
   }
 
   //print Hexadecimal Uint8List
   String printHex() {
     Uint8List bytes = this.toList();
-    return bytes.map((byte) => '0x' + byte.toRadixString(16).padLeft(2, '0') + " ").join();
+    return bytes
+        .map((byte) => '0x' + byte.toRadixString(16).padLeft(2, '0') + " ")
+        .join();
   }
 }
-
-
 
 ///
 /// Class repesents one entire data set in a Data Field. MRTD additional data
@@ -68,12 +67,12 @@ class DataSet {
     rows.add(DataRow(tag: tag, value: value));
   }
 
-  void addRow({required DataRow row}){
+  void addRow({required DataRow row}) {
     rows.add(row);
   }
 
   ///it returns chunk of all data rows as a Uint8List
-  Uint8List toList(){
+  Uint8List toList() {
     var b = BytesBuilder();
     //final bytes = Uint8List(rows.length);
     for (DataRow row in rows) {
@@ -85,7 +84,4 @@ class DataSet {
   void clear() {
     rows.clear();
   }
-
 }
-
-
